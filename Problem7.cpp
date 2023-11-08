518067baa97433b8aad16fc1263f8da3c88d3774
Sk-Shubhasheesh <shubhasheeshkundu02@gmail.com>
2023-11-08 20:26:13 +0530

    Day 4: Move all Zeros to the end of the array

---
+++ Problem7.cpp
@@ -0,0 +1,72 @@
+/*   Problem 7- Move all Zeros to the end of the array
+Problem link - https://www.codingninjas.com/studio/problems/ninja-and-the-zero-s_6581958?utm_source=youtube&utm_medium=affiliate&utm_campaign=striver_Arrayproblems
+*/
+
+// Bruteforce Approach
+#include <bits/stdc++.h>
+using namespace std;
+
+vector<int> moveZeros(int n, vector<int> a) {
+    //temporary array:
+    vector<int> temp;
+    //copy non-zero elements
+    //from original -> temp array:
+    for (int i = 0; i < n; i++) {
+        if (a[i] != 0)
+            temp.push_back(a[i]);
+    }
+
+    // number of non-zero elements.
+    int nz = temp.size();
+
+    //copy elements from temp
+    //fill first nz fields of
+    //original array:
+    for (int i = 0; i < nz; i++) {
+        a[i] = temp[i];
+    }
+
+    //fill rest of the cells with 0:
+    for (int i = nz; i < n; i++) {
+        a[i] = 0;
+    }
+    return a;
+}
+
+
+int main()
+{
+    vector<int> arr = {1, 0, 2, 3, 2, 0, 0, 4, 5, 1};
+    int n = 10;
+    vector<int> ans = moveZeros(n, arr);
+    for (auto &it : ans) {
+        cout << it << " ";
+    }
+    cout << '\n';
+    return 0;
+}
+
+
+
+// Optimal Approach
+vector<int> moveZeros(int n, vector<int> a) {
+    // Write your code here.
+    int j = -1;
+    for(int i =0; i<n; i++){
+        if(a[i] == 0){
+            j = i;
+            break;
+        }
+    }
+    // if array is not containg none zero case
+    if(j == -1){ return a;}
+
+    for(int i = j+1; i<n; i++){
+        if(a[i] != 0){
+            swap(a[i], a[j]);
+            j++;
+        }
+
+    }
+    return a;
+}
