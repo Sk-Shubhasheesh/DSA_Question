e55db307c7d3d7b78ec9c79cf40f725e6ba29375
Sk-Shubhasheesh <shubhasheeshkundu02@gmail.com>
2023-11-14 18:05:55 +0530

    Day 5: find the missing number from the array

---
+++ Problem11.cpp
@@ -0,0 +1,123 @@
+/* Problem - find the missing number from the array
+   Problem link - https://www.codingninjas.com/studio/problems/missing-number_6680467?utm_source=youtube&utm_medium=affiliate&utm_campaign=striver_Arrayproblems&leftPanelTabValue=PROBLEM
+
+
+*/
+
+// Bruteforce Approach
+
+
+
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int missingNumber(vector<int>&a, int N) {
+
+    // Outer loop that runs from 1 to N:
+    for (int i = 1; i <= N; i++) {
+
+        // flag variable to check
+        //if an element exists
+        int flag = 0;
+
+        //Search the element using linear search:
+        for (int j = 0; j < N - 1; j++) {
+            if (a[j] == i) {
+
+                // i is present in the array:
+                flag = 1;
+                break;
+            }
+        }
+
+        // check if the element is missing
+        //i.e flag == 0:
+
+        if (flag == 0) return i;
+    }
+
+    // The following line will never execute.
+    // It is just to avoid warnings.
+    return -1;
+}
+
+int main()
+{
+    int N = 5;
+    vector<int> a = {1, 2, 4, 5};
+    int ans = missingNumber(a, N);
+    cout << "The missing number is: " << ans << endl;
+    return 0;
+}
+
+
+
+// Better Approach
+
+
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int missingNumber(vector<int>&a, int N) {
+
+    int hash[N + 1] = {0}; //hash array
+
+    // storing the frequencies:
+    for (int i = 0; i < N - 1; i++)
+        hash[a[i]]++;
+
+    //checking the freqencies for numbers 1 to N:
+    for (int i = 1; i <= N; i++) {
+        if (hash[i] == 0) {
+            return i;
+        }
+    }
+
+    // The following line will never execute.
+    // It is just to avoid warnings.
+    return -1;
+}
+
+int main()
+{
+    int N = 5;
+    vector<int> a = {1, 2, 4, 5};
+    int ans = missingNumber(a, N);
+    cout << "The missing number is: " << ans << endl;
+    return 0;
+}
+
+
+// Optimal Approach 1
+
+
+int missingNumber(vector<int>&a, int N) {
+
+    //Summation of first N numbers:
+    int sum = (N * (N + 1)) / 2;
+
+    //Summation of all array elements:
+    int s2 = 0;
+    for (int i = 0; i < N - 1; i++) {
+        s2 += a[i];
+    }
+
+    int missingNum = sum - s2;
+    return missingNum;
+}
+
+
+// Optimal Approach 2
+int missingNumber(vector<int>&a, int N) {
+    // Write your code here.
+    int xor1 = 0;
+    int xor2 = 0;
+    for(int i=0; i<N-1; i++){
+        xor1 = xor1^a[i];
+        xor2 = xor2^(i+1);
+    }
+    xor2 = xor2^N;
+    return xor1^xor2;
+}
