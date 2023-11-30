4ab351dde6000b66532af6e2264b2952e9915d44
Sk-Shubhasheesh <shubhasheeshkundu02@gmail.com>
2023-11-30 18:51:41 +0530

    Day 20 - Longest Subarray with given Sum K(Positives)

---
+++ Problem30.cpp
@@ -0,0 +1,43 @@
+// Problem 30 - Longest Subarray with given Sum K(Positives)
+
+
+
+// BruteForce Approche where TC-> O(n3)
+int getLongestSubarray(vector<int>& a, long long k) {
+    int n = a.size(); // size of the array.
+
+    int len = 0;
+    for (int i = 0; i < n; i++) { // starting index
+        for (int j = i; j < n; j++) { // ending index
+            // add all the elements of
+            // subarray = a[i...j]:
+            long long s = 0;
+            for (int K = i; K <= j; K++) {
+                s += a[K];
+            }
+
+            if (s == k)
+                len = max(len, j - i + 1);
+        }
+    }
+    return len;
+}
+
+// Optimize BruteForce Approche where TC-> O(n2)
+int getLongestSubarray(vector<int>& a, long long k) {
+    int n = a.size(); // size of the array.
+
+    int len = 0;
+    for (int i = 0; i < n; i++) { // starting index
+        long long s = 0; // Sum variable
+        for (int j = i; j < n; j++) { // ending index
+            // add the current element to
+            // the subarray a[i...j-1]:
+            s += a[j];
+
+            if (s == k)
+                len = max(len, j - i + 1);
+        }
+    }
+    return len;
+}
