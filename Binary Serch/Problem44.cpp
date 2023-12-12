8405406c9d35d89785981b3b005ce3df75ede4a4
Sk-Shubhasheesh <shubhasheeshkundu02@gmail.com>
2023-12-12 16:21:16 +0530

    Day 31 - Implement upper  bound

---
+++ Binary Serch/Problem44.cpp
@@ -0,0 +1,19 @@
+// Problem 44 - Implement upper bound 
+// Problem link - https://bit.ly/3IoKGce
+
+int upperBound(vector<int> &arr, int x, int n){
+	// Write your code here.
+	int low =0, high = n-1;
+	int ans =n;
+	while(low<=high){
+		int mid = (low+high)/2;
+		if(arr[mid]>x){
+			ans = mid;
+			high = mid-1;
+		}
+		else{
+			low = mid+1;
+		}
+	}
+	return ans;	
+}
