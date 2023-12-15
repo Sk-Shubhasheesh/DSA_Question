144f880f52a2b5a56f3677f3f79999bd828f06e6
Sk-Shubhasheesh <shubhasheeshkundu02@gmail.com>
2023-12-15 20:52:08 +0530

    Day 34- Koko Eating Bananas

---
+++ Binary Serch/Problem57.cpp
@@ -0,0 +1,38 @@
+//Problem 57 - Koko Eating Bananas
+// Problem link - https://bit.ly/41ExJ4K
+#include <bits/stdc++.h>
+int findMax(vector<int> &v) {
+    int maxi = INT_MIN;
+    int n = v.size();
+    //find the maximum:
+    for (int i = 0; i < n; i++) {
+        maxi = max(maxi, v[i]);
+    }
+    return maxi;
+}
+int calculateTotalHours(vector<int> &v, int hourly) {
+    int totalH = 0;
+    int n = v.size();
+    //find total hours:
+    for (int i = 0; i < n; i++) {
+        totalH += ceil((double)(v[i]) / (double)(hourly));
+    }
+    return totalH;
+}
+int minimumRateToEatBananas(vector<int> v, int h) {
+    // Write Your Code Here
+    int low = 1, high = findMax(v);
+
+    //apply binary search:
+    while (low <= high) {
+        int mid = (low + high) / 2;
+        int totalH = calculateTotalHours(v, mid);
+        if (totalH <= h) {
+            high = mid - 1;
+        }
+        else {
+            low = mid + 1;
+        }
+    }
+    return low;
+}
