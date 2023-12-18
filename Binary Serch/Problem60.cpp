636f8da2a22412cab91872bad117d62b2f54aa4c
Sk-Shubhasheesh <shubhasheeshkundu02@gmail.com>
2023-12-18 14:37:53 +0530

    Day 38-Capacity to Ship Packages within D Days

---
+++ Binary Serch/Problem60.cpp
@@ -0,0 +1,85 @@
+// Problem 60 - Capacity to Ship Packages within D Days
+// Problem link - https://bit.ly/3pJg6n4
+
+
+// Bruteforce using linear serch
+#include <bits/stdc++.h>
+using namespace std;
+
+int findDays(vector<int> &weights, int cap) {
+    int days = 1; //First day.
+    int load = 0;
+    int n = weights.size(); //size of array.
+    for (int i = 0; i < n; i++) {
+        if (load + weights[i] > cap) {
+            days += 1; //move to next day
+            load = weights[i]; //load the weight.
+        }
+        else {
+            //load the weight on the same day.
+            load += weights[i];
+        }
+    }
+    return days;
+}
+
+int leastWeightCapacity(vector<int> &weights, int d) {
+    //Find the maximum and the summation:
+    int maxi = *max_element(weights.begin(), weights.end());
+    int sum = accumulate(weights.begin(), weights.end(), 0);
+
+    for (int i = maxi; i <= sum; i++) {
+        if (findDays(weights, i) <= d) {
+            return i;
+        }
+    }
+    //dummy return statement:
+    return -1;
+}
+
+int main()
+{
+    vector<int> weights = {5, 4, 5, 2, 3, 4, 5, 6};
+    int d = 5;
+    int ans = leastWeightCapacity(weights, d);
+    cout << "The minimum capacity should be: " << ans << "\n";
+    return 0;
+}
+
+
+
+
+
+// Optimal using Binary Serch
+int findDays(vector<int> &weights, int cap){
+    int days = 1;
+    int load = 0;
+    for(int i=0; i<weights.size(); i++){
+        if(weights[i]+load > cap){
+            days = days+1;
+            load = weights[i];
+        }else{
+            load += weights[i];
+        }
+    }
+    return days;
+}
+int leastWeightCapacity(vector<int> &weights, int d)
+{
+    //Find the maximum and the summation:
+    int low = *max_element(weights.begin(), weights.end());
+    int high = accumulate(weights.begin(), weights.end(), 0);
+    while (low <= high) {
+        int mid = (low + high) / 2;
+        int numberOfDays = findDays(weights, mid);
+        if (numberOfDays <= d) {
+            //eliminate right half
+            high = mid - 1;
+        }
+        else {
+            //eliminate left half
+            low = mid + 1;
+        }
+    }
+    return low;
+}
