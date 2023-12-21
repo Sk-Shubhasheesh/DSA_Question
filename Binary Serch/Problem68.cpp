add076f904f9a315a1edc7267b730a5acb7bb3e1
Sk-Shubhasheesh <shubhasheeshkundu02@gmail.com>
2023-12-21 20:55:12 +0530

    Day 41-Search in a sorted 2D matrix

---
+++ Binary Serch/Problem68.cpp
@@ -0,0 +1,29 @@
+// Problem 68 - Search in a sorted 2D matrix
+// Problem link - https://www.codingninjas.com/studio/problems/search-in-a-2d-matrix_980531?utm_source=youtube&utm_medium=affiliate&utm_campaign=codestudio_Striver_BinarySeries&leftPanelTab=1
+
+
+
+#include <bits/stdc++.h>
+using namespace std;
+
+bool searchMatrix(vector<vector<int>>& matrix, int target) {
+    int n = matrix.size();
+    int m = matrix[0].size();
+
+    //apply binary search:
+    int low = 0, high = n * m - 1;
+    while (low <= high) {
+        int mid = (low + high) / 2;
+        int row = mid / m, col = mid % m;
+        if (matrix[row][col] == target) return true;
+        else if (matrix[row][col] < target) low = mid + 1;
+        else high = mid - 1;
+    }
+    return false;
+}
+
+int main()
+{
+    vector<vector<int>> matrix = {{1, 2, 3, 4}, {5, 6, 7, 8}, {9, 10, 11, 12}};
+    searchMatrix(matrix, 8) == true ? cout << "true\n" : cout << "false\n";
+}
