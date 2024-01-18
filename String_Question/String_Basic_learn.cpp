8517e12a247e18042a816b532a21624607fc3d87
Sk-Shubhasheesh <shubhasheeshkundu02@gmail.com>
2024-01-18 11:23:07 +0530

    Day 63 - String Basic

---
+++ String_Question/String_Basic_learn.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+using namespace std;
+int main(){
+    string s = "Sk";
+    cout<<s<<endl;
+    //Taking user input
+    string s1;
+    cin>>s1;
+    cout<<s1;
+    // finding string size
+    cout<<s1.size()<<endl;
+
+    // Taking input as a sentence
+   string s3;
+   getline(cin, s3);
+   cout<<s3<<endl;
+
+   // Priniting the sentence
+   string sentence = "Hello ji to \"Kaise\" ha app sb";
+   cout<<sentence<<endl;
+
+}
